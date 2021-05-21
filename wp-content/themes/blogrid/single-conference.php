e2c2ec907f318ddb87b727271129bba68992f376
ETombuyses <e.tombuyses@gmail.com>
2021-05-21 11:17:23 +0200

    [add] conference page : add games linked to conference

--- wp-content/themes/blogrid/single-conference.php
+++ wp-content/themes/blogrid/single-conference.php
@@ -13,13 +13,28 @@ while( have_posts()) {
 
     the_field('titre');
 
-    if( get_field('jeux') ) {
-        the_field('jeux');
-    }
 
-    the_field('description');
-    echo '</br>';
-    ?>
+// GAMES linked to the conference
+    $featured_posts = get_field('jeux');
+    if( $featured_posts ): ?>
+        <ul>
+        <?php foreach( $featured_posts as $post ): 
+    
+            // Setup this post for WP functions (variable must be named $post).
+            setup_postdata($post); ?>
+            <li>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                <span>A custom field from this post: <?php the_field( 'editeur' ); ?></span>
+            </li>
+        <?php endforeach; ?>
+        </ul>
+        <?php 
+        // Reset the global post object so that the rest of the page works correctly.
+        wp_reset_postdata(); ?>
+    <?php endif; ?>
+ <!-- END GAMES linked to the conference -->
+
+
 
     <h2>Fiche technique</h2>
 
